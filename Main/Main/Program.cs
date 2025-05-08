ed4ac75681c7fed792f9e9287ab7927ea28f348d
Vlad119 <unstarkble12@gmail.com>
2025-05-08 18:29:54 +0300

    Создал программу для поиска индексов нужного символа в строке

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,26 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
-        }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string target = Console.ReadLine();
+        char symbol = char.Parse(Console.ReadLine());
+        string result = string.Join(", ", IndexOfAll(target, symbol));
+        Console.WriteLine(result);
     }
+
+    static List<int> IndexOfAll(string target, char symbol)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < target.Length; i++)
+        {
+            if (target[i] == symbol)
+            {
+                result.Add(i);
+            }
+        }
+        return result;
+    }
+}
