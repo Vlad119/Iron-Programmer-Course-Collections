9e2b37dfd465b674aa612088959acea409014743
Vlad119 <unstarkble12@gmail.com>
2025-05-11 13:16:41 +0300

    Сделал задачу 2_2_4

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,23 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System.Collections.Generic;
+using System;
 
-        }
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        List<int> divisors = GetDivisors(num);
+        Console.Write(String.Join(", ", divisors));
     }
+
+    static List<int> GetDivisors(int n)
+    {
+        List<int> result = new List<int>();
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0) result.Add(i);
+        }
+        return result;
+    }
+}
