02103ce8f6d2c3f459283854147465d58cdc0a57
Vlad119 <unstarkble12@gmail.com>
2025-05-11 12:01:29 +0300

    Сделал задачу 2_2_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,34 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> allCources = new List<string>();
+        List<string> passedCources = new List<string>();
+        int totalCourseCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < totalCourseCount; i++)
+        {
+            allCources.Add(Console.ReadLine());
+        }
+        int passedCourseCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < passedCourseCount; i++)
+        {
+            passedCources.Add(Console.ReadLine());
+        }
+        if (allCources.Count == passedCources.Count)
+            Console.WriteLine("Начните новый курс Иосифа Дзеранова! И станьте еще на шаг ближе к профессии разработчик!");
+        else
+        {
+            for (int i = 0; i < passedCourseCount; i++)
+            {
+                allCources.Remove(passedCources[i]);
+            }
+            foreach (var cource in allCources)
+            {
+                Console.WriteLine(cource);
+            }
         }
     }
+}
