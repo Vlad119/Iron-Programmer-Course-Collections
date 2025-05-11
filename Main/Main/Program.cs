f74e222166ab699fb54b7696ca18b995bfaddf50
Vlad119 <unstarkble12@gmail.com>
2025-05-11 11:28:47 +0300

    Сделал задачу 2_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,30 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<string> books = new List<string>();
+        for (int i = 0; i < n; i++)
+        {
+            books.Add(Console.ReadLine());
+        }
+        string newBook = Console.ReadLine();
+        int position = int.Parse(Console.ReadLine());
+        books.Insert(position - 1, newBook);
+        string removedBook = Console.ReadLine();
+        books.Remove(removedBook);
+        string searchBook = Console.ReadLine();
+        int index = books.IndexOf(searchBook);
+        if (index != -1)
+        {
+            Console.WriteLine(index + 1);
+        }
+        else
+        {
+            Console.WriteLine(0);
         }
     }
+}
