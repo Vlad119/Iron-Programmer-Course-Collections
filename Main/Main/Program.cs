7f9bc38d3ee1a163dc59c9a5bd981d57054a7e45
Vlad119 <unstarkble12@gmail.com>
2025-05-12 20:12:59 +0300

    Сделал задачу 2_2_6

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,39 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System.Collections.Generic;
+using System;
 
-        }
+internal class Program
+{
+
+    static void Main(string[] args)// задача на простые делители числа
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        List<int> divisors = GetPrimeDivisors(num);
+        Console.Write(string.Join(" ", divisors));
     }
+
+    static List<int> GetPrimeDivisors(int n)
+    {
+        int count = 0;
+        List<int> result = new List<int>();
+        while (n > 1)
+        {
+            for (int i = 2; i < 10; i++)
+            {
+                count = 0;
+                if (n % i == 0)
+                {
+                    result.Add(i);
+                    n /= i;
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0)
+            {
+                result.Add(n);
+                break;
+            }
+        }
+        return result;
+    }
+}
