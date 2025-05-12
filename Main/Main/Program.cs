e1e9af48bb1a65f5b94a0591d80e4a915ddc68fc
Vlad119 <unstarkble12@gmail.com>
2025-05-12 20:49:03 +0300

    Сделал задачу 2_2_8

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,38 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
-        }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string result = string.Join(" ", GetNumbers(input));
+        Console.WriteLine(result);
     }
+
+    static List<int> GetNumbers(string s)//из текста нужно выбрать только цифры и вывести на экран
+    {
+        List<int> result = new List<int>();
+        string currentNumber = string.Empty; 
+        foreach (char c in s)
+        {
+            if (char.IsDigit(c)) 
+            {
+                currentNumber += c;
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(currentNumber)) 
+                {
+                    result.Add(int.Parse(currentNumber));
+                    currentNumber = string.Empty;
+                }
+            }
+        }
+        if (!string.IsNullOrEmpty(currentNumber))
+        {
+            result.Add(int.Parse(currentNumber));
+        }
+        return result;
+    }
+}
