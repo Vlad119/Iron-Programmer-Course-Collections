767323064bb4dae644bbed5c3be267106d98683c
Vlad119 <unstarkble12@gmail.com>
2025-05-14 20:55:16 +0300

    Сделал задачу 2_3_5

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,23 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main(string[] args)//Задача Иосифа Флавия
+    {
+        List<int> list = new List<int>();
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+        for (int i = 0; i != n; i++)
+        {
+            list.Add(i + 1);
         }
+        int position = 0;
+        while (list.Count > 1)
+        {
+            position = (position + k - 1) % list.Count;
+            list.RemoveAt(position);
+        }
+        Console.WriteLine(list[0]);
     }
+}
