950b8918577a699e7762b1ba4faeb6cc8b285e70
Vlad119 <unstarkble12@gmail.com>
2025-05-15 19:21:35 +0300

    Сделал задачу 2_4_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,21 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
-        }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
     }
+
+    static List<List<string>> Chunked(List<string> list, int n) // создаёт чанки из строки
+    {
+        List<List<string>> result = new List<List<string>>();
+        for (int i = 0; i < list.Count; i += n)
+        {
+            List<string> chunk = list.GetRange(i, Math.Min(n, list.Count - i));
+            result.Add(chunk);
+        }
+        return result;
+    }
+}
