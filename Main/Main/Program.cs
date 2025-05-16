a2fae42253a010138da1dbe6d4c9dda35d08a414
Vlad119 <unstarkble12@gmail.com>
2025-05-16 22:26:47 +0300

    Сделал задачу 3_3_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,20 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main(string[] args)
+    //удаление повторяющихся друг за другом символов и вывод нормальной строки
+    {
+        string[] split = Console.ReadLine().Split();
+        string str = string.Join(" ", split);
+        Stack<char> stack = new Stack<char>();
+        stack.Push(str[0]);
+        foreach (char c in str)
+        {
+            if (c != stack.Peek()) stack.Push(c);
         }
+        stack = new Stack<char>(stack);//переворачиваем стек
+        Console.WriteLine(string.Join("", stack));
     }
+}
