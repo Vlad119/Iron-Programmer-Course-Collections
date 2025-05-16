5303d00f324fd3bc7883e77a4b4086c1970a6e7a
Vlad119 <unstarkble12@gmail.com>
2025-05-16 21:30:02 +0300

    Сделал задачу 3_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,26 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string s = string.Empty;
+        Stack<string> stack = new Stack<string>();
+        while (true)
+        {
+            s = Console.ReadLine();
+            if (s != "Конец списка") stack.Push(s);
+            else break;
+        }
+        while (stack.Count > 0)
+        {
+            if (stack.Peek() == "+")
+            {
+                if (stack.Count != 0) stack.Pop();
+                if (stack.Count != 0) stack.Pop();
+            }
+            else Console.WriteLine(stack.Pop());
         }
     }
+}
