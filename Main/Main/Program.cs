55f41838f8f6276d1b529e7f4c7c8517287dcf64
Vlad119 <unstarkble12@gmail.com>
2025-05-17 13:02:41 +0300

    Сделал задачу 3_3_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,42 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main()
+    {
+        //проверка скобок
+        string input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("YES");
+            return;
+        }
+        Stack<char> stack = new Stack<char>();
+        bool isValid = true;
+        foreach (char c in input)
+        {
+            if (c == '(')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')')
+            {
+                if (stack.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+                stack.Pop();
+            }
+        }
+        if (isValid && stack.Count == 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
         }
     }
+}
