973706950470a34451fa71397c1e1462ec479775
Vlad119 <unstarkble12@gmail.com>
2025-05-17 13:41:50 +0300

    Сделал задачу 3_4_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,51 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main()
+        //проверка на скобки
+    {
+        string input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("True");
+            return;
+        }
+
+        Stack<char> stack = new Stack<char>();
+        bool isValid = true;
+        foreach (char c in input)
+        {
+            if (c == '(' || c == '{' || c == '[' || c == '<')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']' || c == '>')
+            {
+                if (stack.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+                char top = stack.Pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[') ||
+                    (c == '>' && top != '<'))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+        }
+        if (isValid && stack.Count == 0)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
         }
     }
+}
