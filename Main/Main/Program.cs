7c3f33e83ef6d2b4a5c7fa92c4a7db8e0061eb7c
Vlad119 <unstarkble12@gmail.com>
2025-05-20 09:29:55 +0300

    Сделал задачу 4_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,51 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main(string[] args)
+    // задача про очереди в поликлинике
+    {
+        int patientCount = int.Parse(Console.ReadLine());
+        Queue<string> patient = new Queue<string>();
+        for (int i = 0; i < patientCount; i++)
+        {
+            patient.Enqueue(Console.ReadLine());
+        }
+        int windowsCount = int.Parse(Console.ReadLine());
+        int index = 0;
+        if (patient.Count == 0)
+        {
+            for (int i = 0; i < windowsCount; i++)
+            {
+                Console.WriteLine($"Окно {i + 1} свободно");
+            }
+            return;
+        }
+        if (windowsCount < patient.Count)
+        {
+            for (int i = 0; i < windowsCount; i++)
+            {
+                Console.WriteLine($"Клиент {patient.Peek()} --> Окно {i + 1}");
+                patient.Dequeue();
+            }
+            foreach (var p in patient)
+            {
+                Console.WriteLine($"Клиент {p} остается ждать");
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= patient.Count; i++)
+            {
+                Console.WriteLine($"Клиент {patient.Peek()} --> Окно {i + 1}");
+                patient.Dequeue();
+                index++;
+            }
+            for (int i = index; i < windowsCount; i++)
+            {
+                Console.WriteLine($"Окно {i + 1} свободно");
+            }
         }
     }
+}
