41e2ba2f4b315c4b34f62d481d4b50302d376efe
Vlad119 <unstarkble12@gmail.com>
2025-05-22 11:05:36 +0300

    Сделал задачу 5_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,17 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        HashSet<string> cities = new HashSet<string>();
+        for (int i = 0; i < n; i++)
+        {
+            cities.Add(Console.ReadLine());
         }
+        string newCity = Console.ReadLine();
+        Console.WriteLine(cities.Contains(newCity) ? "EXIST" : "OK");
     }
+}
