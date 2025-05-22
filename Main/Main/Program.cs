571b67844b697ea9b0ceeb5eabe5c1c9b6accfb1
Vlad119 <unstarkble12@gmail.com>
2025-05-22 10:56:32 +0300

    Сделал задачу 5_2_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,28 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Считываем количество курсов, которые уже проходит/прошел ученик
+        int n = int.Parse(Console.ReadLine());
+        HashSet<string> completedCourses = new HashSet<string>();
+        for (int i = 0; i < n; i++)
+        {
+            completedCourses.Add(Console.ReadLine());
+        }
+        // Считываем количество курсов, которые ученик наметил пройти
+        int m = int.Parse(Console.ReadLine());
+        List<string> plannedCourses = new List<string>();
+        for (int i = 0; i < m; i++)
+        {
+            plannedCourses.Add(Console.ReadLine());
+        }
+        // Проверяем каждый запланированный курс
+        foreach (string course in plannedCourses)
+        {
+            Console.WriteLine(completedCourses.Contains(course) ? "YES" : "NO");
         }
     }
+}
