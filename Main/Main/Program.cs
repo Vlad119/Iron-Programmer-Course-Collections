5cf11c41d133499990690ee1f7a6eaca92f8428b
Vlad119 <unstarkble12@gmail.com>
2025-05-22 16:16:16 +0300

    Сделал задачу 5_3_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,28 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main()
+    // программа ищет количество студентов, которые изучают только один язык
+    {
+        int n = int.Parse(Console.ReadLine());
+        HashSet<string> cSharpStudents = new HashSet<string>();
+        for (int i = 0; i < n; i++)
+        {
+            cSharpStudents.Add(Console.ReadLine());
         }
+        int m = int.Parse(Console.ReadLine());
+        HashSet<string> kotlinStudents = new HashSet<string>();
+        for (int i = 0; i < m; i++)
+        {
+            kotlinStudents.Add(Console.ReadLine());
+        }
+        HashSet<string> onlyCSharp = new HashSet<string>(cSharpStudents);
+        onlyCSharp.ExceptWith(kotlinStudents);
+        HashSet<string> onlyKotlin = new HashSet<string>(kotlinStudents);
+        onlyKotlin.ExceptWith(cSharpStudents);
+        int result = onlyCSharp.Count + onlyKotlin.Count;
+        Console.WriteLine(result == 0 ? "NO" : result + "");
     }
+}
