5cbf57972f63b548c5a10107544a92088b44c088
Vlad119 <unstarkble12@gmail.com>
2025-05-25 20:01:48 +0300

    Решил задачу 6_2_4

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,33 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main()
+    {
+        //приложение ищет "день сбора"
+        int friendsCount = int.Parse(Console.ReadLine());
+        Dictionary<string, int> meet = new Dictionary<string, int>()
+       {
+            {"пн", 0 },
+            {"вт", 0 },
+            {"ср", 0 },
+            {"чт", 0 },
+            {"пт", 0 },
+            {"сб", 0 },
+            {"вс", 0 }
+        };
+        for (int i = 0; i < friendsCount; i++)
+        {
+            string[] days = Console.ReadLine().Split();
+            foreach (string day in days)
+            {
+                meet[day]++; // у выбранного ключа, увеличиваем счётчик 
+            }
+        }
+        foreach (var day in meet)
+        {
+            if (day.Value != 0) Console.WriteLine(day.Key + " " + day.Value);
         }
     }
+}
