9d30fc77d4206fafc24ef5a23b8aff3d1d985a64
Vlad119 <unstarkble12@gmail.com>
2025-05-25 15:58:34 +0300

    Сделал задачу 6_1_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,43 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System.Collections.Generic;
+using System;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Словарь 1
+        Dictionary<int, string> dictionary1 = new Dictionary<int, string>()
+        {
+            [1] = "Val1",
+            [3] = "Val3",
+            [5] = "Val5"
+        };
+        // Словарь 2
+        Dictionary<int, string> dictionary2 = new Dictionary<int, string>()
+        {
+            [1] = "Val11",
+            [2] = "Val2",
+            [4] = "Val4"
+        };
+        // Объединение словарей
+        Dictionary<int, string> combinedDictionary = CombineDictionaries(dictionary1, dictionary2);
+        // Вывод результата
+        foreach (var item in combinedDictionary)
+        {
+            Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
         }
     }
+
+    static Dictionary<int, string> CombineDictionaries(Dictionary<int, string> dict1, Dictionary<int, string> dict2)
+    {
+        Dictionary<int, string> result = new Dictionary<int, string>(dict1);
+        foreach (var item in dict2)
+        { 
+            if (!result.ContainsKey(item.Key))
+            {
+                result.Add(item.Key, item.Value);
+            }
+        }
+        return result;
+    }
+}
