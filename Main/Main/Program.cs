10d90f76a3ec6d03a9d5772b0d72636464361188
Vlad119 <unstarkble12@gmail.com>
2025-05-25 16:21:26 +0300

    решил задачу 6_2_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,20 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main()
+        //программа ищет синонимы
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string, string> synonyms = new Dictionary<string, string>();
+        for (int i = 0; i < n; i++)
+        {
+            string[] pair = Console.ReadLine().Split();
+            synonyms.Add(pair[0], pair[1]);
+            synonyms.Add(pair[1], pair[0]);
         }
+        string word = Console.ReadLine();
+        Console.WriteLine(synonyms[word]);
     }
+}
