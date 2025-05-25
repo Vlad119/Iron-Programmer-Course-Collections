6c8daf8eea6cd27a09fd5aa639e363148cd70b72
Vlad119 <unstarkble12@gmail.com>
2025-05-25 16:39:02 +0300

    Сделал задачу 6_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,31 @@
-﻿    internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+        //приложение находит страну по городу
     {
-        static void Main(string[] args)
+        int countryCount = int.Parse(Console.ReadLine());
+        Dictionary<string, string> cities = new Dictionary<string, string>();
+        for (int i = 0; i < countryCount; i++)
         {
+            string[] split = Console.ReadLine().Split();
+            for (int j = 1; j < split.Length; j++)
+            {
+                cities.Add(split[j], split[0]);
+            }
+        }
+        int n = int.Parse(Console.ReadLine());
+        string[] split2 = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            split2[i] = Console.ReadLine();
+        }
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(cities[split2[i]]);
 
         }
     }
+}
