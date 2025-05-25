6a1ca5750ea6aaa3e2978b3be2f565ab15492532
Vlad119 <unstarkble12@gmail.com>
2025-05-25 17:00:25 +0300

    Сделал задачу 6_2_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,28 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main()
+
+    {
+        string[] identifiers = Console.ReadLine().Split();
+        Dictionary<string, int> counts = new Dictionary<string, int>();
+        List<string> result = new List<string>();
+        foreach (string str in identifiers)
+        {
+            if (!counts.ContainsKey(str))
+            {
+                result.Add(str);
+                counts[str] = 1;
+            }
+            else
+            {
+                int count = counts[str];
+                result.Add($"{str}_{count}");
+                counts[str]++;
+            }
         }
+        Console.WriteLine(string.Join(" ", result));
     }
+}
