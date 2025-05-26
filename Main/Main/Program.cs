1a20f84ba8d1673f9ad8fcb185c768688c83c983
Vlad119 <unstarkble12@gmail.com>
2025-05-26 12:09:27 +0300

    Решил задачу 6_3_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,25 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main()
+    {
+        string[] split = Console.ReadLine().ToLower().Split();
+        Dictionary<string, int> words = new Dictionary<string, int>();
+        string s = string.Empty;
+        foreach (var str in split)
+        {
+            if (words.ContainsKey(str))
+            {
+                words[str]++;
+            }
+            else
+            {
+                words.Add(str, 1);
+            }
+            s += words[str] + " ";
         }
+        Console.Write($"{s} ");
     }
+}
