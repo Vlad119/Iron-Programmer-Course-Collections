7126d5ad602d0bef457271b9644761c16a266a79
Vlad119 <unstarkble12@gmail.com>
2025-05-26 13:38:57 +0300

    Решил задачу 6_3_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,34 @@
-﻿    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
 
+public class MainClass
+{
+    public static void Main()
+    {
+        var word = Console.ReadLine();
+        var dict1 = new Dictionary<char, int>();
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (dict1.ContainsKey(word[i]))
+            {
+                dict1[word[i]]++;
+            }
+            else
+            {
+                dict1.Add(word[i], 1);
+            }
+        }
+        int n = Convert.ToInt32(Console.ReadLine());
+        var dict2 = new Dictionary<int, char>();
+        for (int i = 0; i < n; i++)
+        {
+            var str = Console.ReadLine().Split(new string[] { ": " }, StringSplitOptions.None);
+            dict2[Convert.ToInt32(str[1])] = Convert.ToChar(str[0]);
+            dict2.Add(int.Parse(str[1]),char.Parse(str[0]));
+        }
+        foreach (var c in word)
+        {
+            Console.Write(dict2[dict1[c]]);
         }
     }
+}
