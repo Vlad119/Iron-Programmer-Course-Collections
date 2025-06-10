842eddd283f2ad53c68de67077f778ebc294c274
Vlad119 <unstarkble12@gmail.com>
2025-06-10 15:08:03 +0300

    Новая тема

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,2 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
-
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
